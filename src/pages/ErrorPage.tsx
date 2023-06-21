821b5249dfd90cc2b1643c170b5b189d16a1a608
Dustin <dustindung300@gmail.com>
2023-06-21 18:59:45 +0700

    Fetch game details.

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,10 +1,10 @@
 import { Box, Heading, Text } from "@chakra-ui/react";
-import React from "react";
 import { isRouteErrorResponse, useRouteError } from "react-router-dom";
 import NavBar from "../components/NavBar";
 
 const ErrorPage = () => {
   const error = useRouteError();
+
   return (
     <>
       <NavBar />
